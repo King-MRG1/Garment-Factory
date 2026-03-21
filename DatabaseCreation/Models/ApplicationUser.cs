4d8b1f342abeffe756c829be077c9532e5ec1610
Mohamed Ramadan <MohamedRamdan2214@outlook.com>
2026-03-21 15:51:25 +0200

    ~

--- DatabaseCreation/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-namespace DatabaseCreation.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string Full_name { get; set; }
-        List<RefreshTokenStore> RefreshTokens { get; set; }
-    }
-}
