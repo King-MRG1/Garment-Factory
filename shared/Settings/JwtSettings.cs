5d6998e415fa4d00d5ee4e1962f398765f5fdada
King-MRG1 <MohamedRamdan2214@outlook.com>
2026-03-25 17:51:39 +0200

    AddJwtSettings

---
+++ shared/Settings/JwtSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Settings
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public double ExpirationInMinutes { get; set; }
+        public int RefreshTokenExpirationInDays { get; set; }
+    }
+}
